1cb747c98dc69fa5e2c68e88e5bc263f85358d82
recoveryMonster <fengsongting@gmail.com>
2023-02-24 22:02:27 +0800

    chapter2：preliminary learn build

---
+++ chapter2/clone/src/index.js
@@ -0,0 +1,22 @@
+import { type } from "./type";
+export function clone(source) {
+  const t = type(source)
+  if (t !== 'object' || t !== 'array') {
+    return source
+  }
+  let target
+  if (t === 'object') {
+    target = {}
+    for (let i in source) {
+      if (source.hasOwnProprty(i)) {
+        target[i] = clone(source[i])
+      }
+    }
+  } else {
+    target = []
+    for (let i = 0; i < source.length; i++) {
+      target[i] = clone(source[i])
+    }
+  }
+  return target
+}
