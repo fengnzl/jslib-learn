26f6c70d8e647982b79f4556714874730c9bbf5f
recoveryMonster <fengsongting@gmail.com>
2023-03-03 00:48:46 +0800

    chaper6: complete isEqual function by adding enhancer parameter and add test

--- chapter6/isEqual/src/index.js
+++ chapter6/isEqual/src/index.js
@@ -1,43 +1,51 @@
-function getType(val) {
+export function getType(val) {
   return Object.prototype.toString.call(val).slice(8, -1).toLowerCase()
 }
 
-export function isEqual(value, other) {
-  if (value === other) {
-    return true
-  }
-  const vType = getType(value)
-  const oType = getType(other)
+export function isEqual(value, other, enhancer) {
+  const next = () => {
+    if (value === other) {
+      return true
+    }
+    const vType = getType(value)
+    const oType = getType(other)
 
-  if (vType !== oType) {
-    return false
+    if (vType !== oType) {
+      return false
+    }
+
+    if (vType === 'array') {
+      return equalArray(value, other, enhancer)
+    }
+
+    if (vType === 'object') {
+      return equalObject(value, other, enhancer)
+    }
+    return value === other
   }
 
-  if (vType === 'array') {
-    return equalArray(value, other)
+  if (typeof enhancer === 'function') {
+    return enhancer(next)(value, other)
   }
 
-  if (vType === 'object') {
-    return equalObject(value, other)
-  }
-  return value === other
+  return next()
 }
 
-function equalArray(value, other) {
+function equalArray(value, other, enhancer) {
   const vLen = value.length
   const oLen = other.length
   if (vLen !== oLen) {
     return false
   }
   for (let i = 0; i < vLen; i++) {
-    if (!isEqual(value[i], other[i])) {
+    if (!isEqual(value[i], other[i], enhancer)) {
       return false
     }
   }
   return true
 }
 
-function equalObject(value, other) {
+function equalObject(value, other, enhancer) {
   const vKeys = Object.keys(value)
   const oKeys = Object.keys(other)
   if (vKeys.length !== oKeys.length) {
@@ -46,7 +54,7 @@ function equalObject(value, other) {
   for (let i = 0; i < vKeys.length; i++) {
     const v = value[vKeys[i]]
     const o = other[vKeys[i]]
-    if (!isEqual(v, o)) {
+    if (!isEqual(v, o, enhancer)) {
       return false
     }
   }
--- chapter6/isEqual/test/index.spec.js
+++ chapter6/isEqual/test/index.spec.js
@@ -1,7 +1,25 @@
 import { expect, it, describe } from 'vitest'
-import { isEqual } from '../src/index.js'
+import { getType, isEqual } from '../src/index.js'
 
 describe('isEqual', () => {
+  const nanMiddleware = () => {
+    return (next) => (value, other) => {
+      if (getType(value) === 'number' && getType(other) === 'number') {
+        if (Number.isNaN(value) && Number.isNaN(other)) {
+          return true
+        }
+      }
+      return next(value, other)
+    }
+  }
+  const functionMiddleware = () => {
+    return (next) => (value, other) => {
+      if (getType(value) === 'function' && getType(other) === 'function') {
+        return value.toString() === other.toString()
+      }
+      return next(value, other)
+    }
+  }
   describe('same type', () => {
     it('number', () => {
       expect(isEqual(1, 1)).toBe(true)
@@ -32,6 +50,44 @@ describe('isEqual', () => {
     })
     it('null and undefined', () => {
       expect(isEqual(null, undefined)).toBe(false)
+    })
+  })
+  describe('test enhancer', () => {
+    it('NaN is not equal NaN when lack of nanMiddleware', () => {
+      expect(isEqual(NaN, NaN)).toBe(false)
+    })
+    it('NaN is equal NaN when nanMiddleware exist', () => {
+      expect(isEqual(NaN, NaN, nanMiddleware())).toBe(true)
+    })
+    const a1 = { a: function () {} }
+    const a2 = { a: function () {} }
+    it('function not equal when lack of functionMiddleware', () => {
+      expect(isEqual(a1, a2)).toBe(false)
+    })
+    it('function is equal when functionMiddleware exist', () => {
+      expect(isEqual(a1, a2, functionMiddleware())).toBe(true)
+    })
+  })
+  describe('compose', () => {
+    const compose = (...fns) => {
+      if (fns.length === 0) {
+        return (...args) => args
+      }
+      if (fns.length === 1) {
+        return fns[0]
+      }
+      return fns.reduce(
+        (a, b) =>
+          (...args) =>
+            a(b(...args))
+      )
+    }
+    it('should equal when NaN and function exists', () => {
+      const a1 = { a: function () {}, b: NaN }
+      const a2 = { a: function () {}, b: NaN }
+      expect(
+        isEqual(a1, a2, compose(functionMiddleware(), nanMiddleware()))
+      ).toBe(true)
     })
   })
 })
